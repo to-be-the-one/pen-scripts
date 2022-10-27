c785c9b160efb7167d1945b2a9cb18ae3eaa9c10
cyhfvg <code.guangzhou.zhang@outlook.com>
2022-10-27 17:09:39 +0800

    feat(`gather`): `rpcbind` : **rpcbind**

---
+++ gather/gather-rpcbind-info.sh
@@ -0,0 +1,44 @@
+#!/bin/env bash
+
+:<<!
+auth: @cyhfvg https://github.com/cyhfvg
+date: 2022/10/27
+
+require: rpcclient
+
+invoke rpcclient's enumXXXX command, save output into *.rpcclient file.
+!
+
+if [ $# -lt 1 ]
+then
+    echo -e "\nUsage: $0 <rpcclient_param>\n"
+    echo -e "Example: $0 -N -U thinc.local/uName:pword 10.11.1.8\n"
+    printf "\033[33m-N -U thinc.local/uName:pword 10.11.1.8\033[0m is rpcclient's param.\n\n";
+    exit 0
+fi
+
+param=""
+while [ "$1" != "" ]; do
+    if [ -z "${param}" ]
+    then
+        param="$1"
+    else
+        param="${param} $1"
+    fi
+    shift
+done
+
+rpcclient_enum_cmd=("enumdomains" "enumdrivers" "enumports" "enumprocdatatypes" "enumdomgroups" "enummonitors" "enumprinters" "enumprocs" "enumdomusers" "enumpermachineconnections" "enumprivs" "enumtrust")
+
+for cmd in ${rpcclient_enum_cmd[*]}
+do
+    rpc_client="rpcclient ${param} -c '${cmd}'"
+    printf "\n\033[33m${rpc_client}\033[0m\n"
+
+    res=$(eval ${rpc_client})
+    if [ -n "${res}" ]
+    then
+        echo "${res}" | tee "${cmd}.rpcclient"
+    fi
+done
+
